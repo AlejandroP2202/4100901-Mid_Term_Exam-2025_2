f21ba77291ae41160f0e0911dbb268d87446f211
AlejandroP2202 <lperezmu@unal.edu.co>
2025-10-23 12:54:04 -0500

    Si

--- Src/room_control.c
+++ Src/room_control.c
@@ -19,6 +19,7 @@ void room_control_app_init(void)
 {
     // Inicializar PWM al duty cycle inicial (estado IDLE -> LED apagado)
     tim3_ch1_pwm_set_duty_cycle(PWM_INITIAL_DUTY);
+    uart_send_string("Controlador de sala v2.0 Estado inicial:-Lampara 20% - puerta : cerrada\r\n");
 }
 
 void room_control_on_button_press(void)
@@ -34,7 +35,7 @@ void room_control_on_button_press(void)
         uart_send_string("Sala vacía\r\n");
     }
 }
-
+DutyCycle = GetCurrentTaskCycleCount();
 void room_control_on_uart_receive(char received_char)
 {
     switch (received_char) {
@@ -63,29 +64,84 @@ void room_control_on_uart_receive(char received_char)
             break;
         case '1':
             tim3_ch1_pwm_set_duty_cycle(10);
-            uart_send_string("PWM: 10%\r\n");
+            uart_send_string("Controlador de Sala v2.0Estado inicial:Lámpara: 10% Puerta: Cerrada\r\n");
             break;
         case '2':
             tim3_ch1_pwm_set_duty_cycle(20);
-            uart_send_string("PWM: 20%\r\n");
+            uart_send_string("Controlador de Sala v2.0Estado inicial:Lámpara: 20% Puerta: Cerrada\r\n");
             break;
         case '3':
             tim3_ch1_pwm_set_duty_cycle(30);
-            uart_send_string("PWM: 30%\r\n");
+            uart_send_string("Controlador de Sala v2.0Estado inicial:Lámpara: 30% Puerta: Cerrada\r\n");
             break;
         case '4':
             tim3_ch1_pwm_set_duty_cycle(40);
-            uart_send_string("PWM: 40%\r\n");
+            uart_send_string("Controlador de Sala v2.0Estado inicial:Lámpara: 40% Puerta: Cerrada\r\n");
             break;
         case '5':
             tim3_ch1_pwm_set_duty_cycle(50);
-            uart_send_string("PWM: 50%\r\n");
+            uart_send_string("Controlador de Sala v2.0Estado inicial:Lámpara: 50% Puerta: Cerrada\r\n");
             break;
+        case 's':
+        DutyCycle = GetCurrentTaskCycleCount();
+        uart_send_string("Estado actual: - Lámpara:",DutyCycle,"%-Puerta: Cerrada\r\n");
+        break;
+
         default:
             uart_send_string("Comando desconocido: ");
             uart_send(received_char);
             uart_send_string("\r\n");
             break;
+
+        case '?':
+        uart_send_string("comandos disponibles:\r\n");
+        uart_send_string("1-5: Ajustar brillo lampara ( 10%, 20%, 30%, 40%, 50%)");
+        uart_send_string(" 0: Apagar lampara \r\n");
+        uart_send_string(" o: Abrir puerta (ocupar salar)\r\n");
+        uart_send_string(" c: Cerrar puerta(vaciar sala)\r\n");
+        uart_send_string("s: Estado del sistema \r\n");
+        uart_send_string("?:Ayuda\r\n");
+        case '1':
+            tim3_ch1_pwm_set_duty_cycle(10);
+            uart_send_string("Controlador de Sala v2.0Estado inicial:Lámpara: 10% Puerta: Cerrada\r\n");
+            break;
+        case '2':
+            tim3_ch1_pwm_set_duty_cycle(20);
+            uart_send_string("Controlador de Sala v2.0Estado inicial:Lámpara: 20% Puerta: Cerrada\r\n");
+            break;
+        case '3':
+            tim3_ch1_pwm_set_duty_cycle(30);
+            uart_send_string("Controlador de Sala v2.0Estado inicial:Lámpara: 30% Puerta: Cerrada\r\n");
+            break;
+        case '4':
+            tim3_ch1_pwm_set_duty_cycle(40);
+            uart_send_string("Controlador de Sala v2.0Estado inicial:Lámpara: 40% Puerta: Cerrada\r\n");
+            break;
+        case '5':
+            tim3_ch1_pwm_set_duty_cycle(50);
+            uart_send_string("Controlador de Sala v2.0Estado inicial:Lámpara: 50% Puerta: Cerrada\r\n");
+            break;
+         case 'o':
+            current_state = ROOM_OCCUPIED;
+            tim3_ch1_pwm_set_duty_cycle(100);
+            led_on_time = systick_get_ms();
+            uart_send_string("Sala ocupada\r\n");
+            break;  
+        case 'c':
+            current_state = ROOM_IDLE;
+            tim3_ch1_pwm_set_duty_cycle(0);
+            uart_send_string("Sala vacía\r\n");
+            break; 
+        case 's':
+        DutyCycle = GetCurrentTaskCycleCount();
+        uart_send_string("Estado actual: - Lámpara:"DustyCycle"%-Puerta: Cerrada\r\n");
+        break;
+        case '0':
+            current_state = ROOM_IDLE;
+            tim3_ch1_pwm_set_duty_cycle(0);
+            uart_send_string("Sala vacía\r\n"); 
+            break;
+
     }
 }
 
